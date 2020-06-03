5a1b17fda32b4d5281cb87d9718bf0feb30e949c
David Rincon <david.rincon.morales@outlook.com>
2020-06-03 18:02:29 -0500

    useSatet() Hook for State Manipulation

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,22 +1,24 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from './Person/Person';
 
-class App extends Component {
-
-  state = {
+const App = props => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       {name: "David", age : 21},
       {name: "Karen", age : 23},
       {name: "Caro", age : 16}
-    ],
-    otherState: "Some other value"
-  };
+    ]
+  });
 
-  switchNameHandler = () => {
+  const [otherState, setOtherState] = useState("Some other value");
+
+  console.log(personsState, otherState)
+
+  const switchNameHandler = () => {
     //console.log("Button Was Clicked");
     // DO NOT DO THIS: this.state.persons[0].name = "Deivis";
-    this.setState({
+    setPersonsState({
       persons: [
       {name: "Deivis", age : 21},
       {name: "Karen", age : 23},
@@ -25,19 +27,17 @@ class App extends Component {
     })
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I´m a React App</h1>
-        <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>MyHobbies: Work</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    );
+  return (
+    <div className="App">
+      <h1>Hi, I´m a React App</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>MyHobbies: Work</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
     // return React.createElement('div', {className:"App"}, React.createElement('h1', null, "does this work now"));
-  }
 }
 
 export default App;
